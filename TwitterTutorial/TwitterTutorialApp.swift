91e6a2ae5e54d6ca4e13bc9d8b8538fa031276a3
Oncu <oohancan@gmail.com>
2023-01-14 13:20:15 +0300

    Initial Commit

---
+++ TwitterTutorial/TwitterTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterTutorialApp.swift
+//  TwitterTutorial
+//
+//  Created by Oncu Can on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
